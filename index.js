7829ee7eb8713c160d9697c8c6d67404a2072d07
9Moses <esselmoses12@gmail.com>
2023-09-28 12:17:35 +0000

    Backend for lizzy print press and database is pg and env is node

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const authRoutes = require("../backend/src/routes/auth");
+const cors = require("cors");
+
+const app = express();
+const port = 3000; // You can change this to your preferred port
+require("dotenv").config();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${process.env.PORT}`);
+});
